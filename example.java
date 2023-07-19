69408bc053dbdd5a2736dac9cebf4c5cd85dd982
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-07-19 11:56:32 +0200

    Update example.java

--- example.java
+++ example.java
@@ -1,13 +1,13 @@
 /**
-* This method always returns immediately, whether or not the 
-* image exists. When this applet attempts to draw the image on
-* the screen, the data will be loaded. The graphics primitives 
-* that draw the image will incrementally paint on the screen. 
-* @param  url  an absolute URL giving the base location of the image
-* @param  name the location of the image, relative to the url argument
-* @return  the image at the specified URL
-* @see Image
-*/
+ * Solves equations of the form a * x = b
+ * @example
+ * // returns 2
+ * globalNS.method1(5, 10);
+ * @example
+ * // returns 3
+ * globalNS.method(5, 15);
+ * @returns {Number} Returns the value of x for the equation.
+ */
 
 public Image getImage(URL url, String name) {
 try {
