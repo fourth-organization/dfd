55aee0851498cf488bed585e202afbce5a673501
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-09-03 23:21:05 +0200

    Update Linear.rb

--- Linear.rb
+++ Linear.rb
@@ -3,11 +3,5 @@
 =begin
 Every body mentioned this way
 to have multiline comments.
-
-=end=begin
-Every body mentioned this way
-to have multiline comments.
-
-The =begin and =end must be at the beginning of the line or
-it will be a syntax error.
 =end
+
