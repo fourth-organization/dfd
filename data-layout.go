e7ab59d43471ef5fa4051b2dce3ce5f3613d1187
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-09-06 13:32:09 +0200

    Update data-layout.go

--- data-layout.go
+++ data-layout.go
@@ -1,3 +1,10 @@
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
 /*
 Gofmt formats Go programs.
 It uses tabs for indentation and blanks for alignment.
