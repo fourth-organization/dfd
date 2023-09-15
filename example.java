1bced4fb54a520408423b087c3e329df351653d7
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-09-15 09:50:08 +0200

    Update example.java

--- example.java
+++ example.java
@@ -11,8 +11,12 @@
 
 public Image getImage(URL url, String name) {
 try {
+return null;
+}
+
+  public Image getImage(URL url, String name) {
+try {
 return getImage(new URL(url, name));
 } catch (MalformedURLException e) {
 return null;
-}
 }
