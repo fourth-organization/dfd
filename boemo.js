7708cff7d53a340d17f6aa2f791f2cb336634591
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-10-26 12:52:18 +0200

    Update boemo.js

--- boemo.js
+++ boemo.js
@@ -2,7 +2,7 @@
  * This is a function.
  *
  * @param {string} n - A string param
- * @param {string} [o] - A optional string param
+ * @param {stringdvdf} [o] - A optional string param
  * @param {string} [d=DefaultValue] - A optional string param
  * @return {string} A good string
  *
